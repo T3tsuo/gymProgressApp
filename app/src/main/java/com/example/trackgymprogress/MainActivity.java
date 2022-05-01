e2411221c59f98504897240828709d9d55716dc2
Takumi Comeau <comeautakumi@gmail.com>
2022-05-01 00:58:04 -0300

    Added a local backup and restore feature.

--- app/src/main/java/com/example/trackgymprogress/MainActivity.java
+++ app/src/main/java/com/example/trackgymprogress/MainActivity.java
@@ -1,15 +1,34 @@
 package com.example.trackgymprogress;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+import android.os.Environment;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
     private WorkoutDayClass workoutDay;
+    private FirebaseDatabase FBDatabase;
+    private DatabaseReference allDB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +39,8 @@ public class MainActivity extends AppCompatActivity {
         Button pushBtn = findViewById(R.id.pushBtn);
         Button pullBtn = findViewById(R.id.pullBtn);
         Button legsBtn = findViewById(R.id.legsBtn);
+        Button backupBtn = findViewById(R.id.backupBtn);
+        Button restoreBtn = findViewById(R.id.restoreBtn);
 
         pushBtn.setOnClickListener(view -> {
             workoutDay.setPush();
@@ -33,6 +54,63 @@ public class MainActivity extends AppCompatActivity {
             workoutDay.setLegs();
             switchActivity();
         });
+        backupBtn.setOnClickListener(view -> getAllWorkouts());
+        restoreBtn.setOnClickListener(view -> restoreAllWorkouts());
+    }
+
+    public void restoreAllWorkouts() {
+        FBDatabase = FirebaseDatabase.getInstance(getString(R.string.FIREBASE_URL));
+        allDB = FBDatabase.getReference();
+        String jsonString = readFromFile("workout_data_backup.txt");
+        Map<String, Object> jsonMap = new Gson().fromJson(jsonString,
+                new TypeToken<HashMap<String, Object>>() {}.getType());
+        allDB.updateChildren(jsonMap);
+    }
+
+    public String readFromFile(String fileName) {
+        File path = getApplicationContext().getFilesDir();
+        File readFrom = new File(path, fileName);
+        byte[] content = new byte[(int) readFrom.length()];
+        try {
+            FileInputStream stream = new FileInputStream(readFrom);
+            stream.read(content);
+            stream.close();
+            Toast.makeText(this, "Restored", Toast.LENGTH_SHORT).show();
+            return new String(content);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return e.toString();
+        }
+    }
+
+    public void getAllWorkouts() {
+        FirebaseDatabase FBDatabase = FirebaseDatabase.getInstance(getString(R.string.FIREBASE_URL));
+        allDB = FBDatabase.getReference();
+        allDB.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                Object object = snapshot.getValue(Object.class);
+                String json = new Gson().toJson(object);
+                writeToFile("workout_data_backup.txt", json);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    public void writeToFile(String fileName, String content) {
+        File path = getApplicationContext().getFilesDir();
+        try {
+            FileOutputStream writer = new FileOutputStream(new File(path, fileName));
+            writer.write(content.getBytes());
+            writer.close();
+            Toast.makeText(this, "Backed up", Toast.LENGTH_SHORT).show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void switchActivity() {
